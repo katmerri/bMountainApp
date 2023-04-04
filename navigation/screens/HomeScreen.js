e071bb0bdd3d7d9cdc545fabf208475407e5b178
katmerri <you@example.com>
2023-04-03 22:49:35 -0400

    Season Product Link

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,13 +1,26 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, Linking, Alert } from 'react-native';
 
+const seasonPasses = 'https://www.bristolmountain.com/season-products/';
+
+const OpenUrlButton = ({url, children}) => {
+    const handlePress = React.useCallback(async () =>{
+        const supported = await Linking.canOpenURL(url);
+
+        if (supported) {
+            await Linking.openURL(url);
+        } else {
+            Alert.alert('Do not know how to open URL: ${url}');
+        }
+    }, [url]);
+
+    return <Button title={children} onPress={handlePress}/>;
+};
 
 export default function HomeScreen({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'top' }}>
-            <Text>
-                Trails Open:
-            </Text>
+            <OpenUrlButton url={seasonPasses}>2023-24 Season Products</OpenUrlButton>
         </View>
 
     );
--- navigation/screens/TrailMapScreen.js
+++ navigation/screens/TrailMapScreen.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 
+
+
 export default function TrailMapScreen({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text 
-            onPress={() => navigation.navigate('Home')} 
-            style={{ fontSize: 26, fontWeight: 'bold' }}> Trail Map Screen</Text>
+        <View>
+            <Text>Trail Map</Text>
         </View>
-
+        
     );
 }
+
